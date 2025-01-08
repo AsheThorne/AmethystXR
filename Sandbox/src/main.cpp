59efe987296e407b20cd22121c89988f9223570c
Ashe <AsheThorne30@gmail.com>
2025-01-09 00:49:01 +1100

    Removed OpenXR-Hpp

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -11,7 +11,7 @@
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
     const auto engineSetupConfig = axr::SetupConfig(axr::LogLevelEnum::Info);
     axr::setup(engineSetupConfig);
-    
+
     axr::loggerSetup("Sandbox");
     axr::logInfo("Test");
 
@@ -19,7 +19,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         "Sandbox",
         AXR_MAKE_VERSION(1, 0, 0)
     );
-    
+
     auto app = axr::Application(appConfig);
     app.setup();
 }
